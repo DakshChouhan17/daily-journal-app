1f95b742842994c84b150e869272610696e4208e
dakshchouhan17 <dakshchouhan8705@gmail.com>
2025-06-21 20:43:17 +0530

    Frontend setup for deployment

--- daily-journal-backend/server.js
+++ daily-journal-backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/journals', journalRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 connectDB().then(() => {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
